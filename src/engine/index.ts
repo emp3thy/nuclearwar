66fed81de24dd34da4706ebe11b45b312ded6fa5
gethi <emp3thy@googlemail.com>
2026-05-09 21:10:46 +0100

    engine: add public barrel export

---
+++ src/engine/index.ts
@@ -0,0 +1,15 @@
+export * from './types';
+export { initialState } from './state';
+export { reduce } from './reducer';
+export { resolveRound } from './resolution';
+export { apCostOf, totalApCost, validateOrder } from './orders';
+export {
+  LEADER_PROFILES,
+  ACTION_COSTS,
+  YIELD_DAMAGE,
+  FACTORY_AP_RATE,
+  AP_BANK_CAP,
+  PROPAGANDA_TRANSFER_M,
+  WOO_FAVOURABILITY_DECAY,
+  DOMINANCE_THRESHOLD_DEFAULT,
+} from './balance';
